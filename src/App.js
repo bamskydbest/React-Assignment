13ee7f07af34f6122e116b6d8430ac0ce5c5779f
User <user@Mahmoud-MacBook-Pro.local>
2025-01-29 13:55:13 +0100

    javascript

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  // Data Types
+
+  // Addition (+)
+
+  const addition = 3 + 5;
+  const view = 50 + 40;
+  const check = addition + view;
+  console.log(addition, view, check);
+
+  // Subtraction (-)
+
+  const takeAway = 100 - 27;
+  const weUp = 10000 - 23000;
+  console.log(takeAway, weUp);
+  return <div></div>;
+}
+
+export default App;
