81936279d7a961b1f481e198c1a4433de1ed7bce
User <user@Mahmoud-MacBook-Pro.local>
2025-02-24 17:39:44 +0100

    Changed from fruit to books

--- src/components/SearchFiltering.js
+++ src/components/SearchFiltering.js
@@ -3,21 +3,52 @@ import React, { useState } from "react";
 const SearchFiltering = () => {
   const [search, setSearch] = useState("");
   const items = [
-    { id: 1, name: "Apple", category: "Fruit" },
-    { id: 2, name: "Banana", category: "Fruit" },
-    { id: 3, name: "Carrot", category: "Fruit" },
-    { id: 4, name: "Daikon", category: "Vegetable" },
-    { id: 5, name: "Eggplant", category: "Vegetable" },
-    { id: 6, name: "Fig", category: "Fruit" },
-    { id: 7, name: "Grape", category: "Fruit" },
+    { id: 1, title: "Alien Corn", author: "Sidney Howard", genre: "Adventure" },
+    {
+      id: 2,
+      title: "After Many a Summer Dies the Swan",
+      author: "Aldous Huxley",
+      genre: "Adventure",
+    },
+    {
+      id: 3,
+      title: "Captains Courageous",
+      author: "Rudyard Kipling",
+      genre: "Adventure",
+    },
+    {
+      id: 4,
+      title: "The Daffodil Sky	",
+      author: "H. E. Bates",
+      genre: "Sci-fi",
+    },
+    {
+      id: 5,
+      title: "Endless Night	",
+      author: "Agatha Christie",
+      genre: "Sci-fi",
+    },
+    {
+      id: 6,
+      title: "A Farewell to Arms	",
+      author: "Ernest Hemingway",
+      genre: "Sci-fi",
+    },
+    {
+      id: 7,
+      title: "The Green Bay Tree	",
+      author: "Louis Bromfield",
+      genre: "Sci-fi",
+    },
   ];
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
   const filteredList = items.filter((item) => {
     return (
-      item.name.toLowerCase().includes(search) ||
-      item.category.toLowerCase().includes(search)
+      item.title.toLowerCase().includes(search) ||
+      item.author.toLowerCase().includes(search) ||
+      item.genre.toLowerCase().includes(search)
     );
   });
   return (
@@ -26,7 +57,7 @@ const SearchFiltering = () => {
       <br />
       {filteredList.map((item) => (
         <p key={item.id}>
-          {item.name}: {item.category}
+          {item.title}: {item.author} - {item.genre}
         </p>
       ))}
     </div>
